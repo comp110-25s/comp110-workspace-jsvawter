22dee5d3dd8e0ab8b5ae259c8bb5ea2e0d33da52
John Vawter <jsvawter@ad.unc.edu>
2025-01-26 16:24:22 -0500

    Added class notes in welcome.py

--- welcome.py
+++ welcome.py
@@ -5,3 +5,12 @@ __author__ = "730597830"
 print("Welcome to COMP110!")
 print("You are in for a fun adventure into programming!")
 print("<3 the COMP110 Team!")
+
+
+def celsius_to_fahrenheit(degrees: int) -> float:
+    """convert degrees cesius to fahrenheit"""
+    return (degrees * 9 / 5) + 32
+
+
+def perimeter(length: float, width: float) -> float:
+    return (length + width) * 2
