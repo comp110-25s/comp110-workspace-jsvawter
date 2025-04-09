98aa366ed8587eb6eed5b2991e35d0fa8fce67cf
John Vawter <jsvawter@ad.unc.edu>
2025-04-09 16:10:20 -0400

    Created my_river object and defined view_river method

--- exercises/ex04/river_simulation.py
+++ exercises/ex04/river_simulation.py
@@ -1,1 +1,22 @@
+"""File to define River class."""
 
+from exercises.ex04.fish import Fish
+from exercises.ex04.bear import Bear
+
+
+class my_river:
+    fish1: Fish
+    fish2: Fish
+    fish3: Fish
+    fish4: Fish
+    fish5: Fish
+    fish6: Fish
+    fish7: Fish
+    fish8: Fish
+    fish9: Fish
+    fish10: Fish
+    bear1: Bear
+    bear2: Bear
+
+    def view_river(self):
+        return None
