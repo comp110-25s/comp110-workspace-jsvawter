ab9da8826a2f9d86ad8713b835628d1dd3d04978
John Vawter <jsvawter@ad.unc.edu>
2025-04-09 16:06:34 -0400

    Added Fish attributes added values to __init__

--- exercises/ex04/fish.py
+++ exercises/ex04/fish.py
@@ -1,9 +1,12 @@
 """File to define Fish class."""
 
+
 class Fish:
-    
-    def __init__(self):
+    age: int
+
+    def __init__(self, age: int):
+        self.age = 0
         return None
-    
+
     def one_day(self):
         return None
